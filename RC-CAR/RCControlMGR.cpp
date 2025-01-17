16c1f71f89154b585696352be30014fb422b05b7
Davide_24 <009davide@gmail.com>
2025-01-17 11:44:29 +0100

    Update the motor controls to be easier to work with with the robocar class

--- RC-CAR/RCControlMGR.cpp
+++ RC-CAR/RCControlMGR.cpp
@@ -3,46 +3,56 @@
 
 namespace RCCar::Utils 
 {
-	int lerp(int a, int b, float f)
+	double lerp(double a, double b, double f)
 	{
-		return (int)((float)a + f * ((float)b - (float)a));
+		return a + f * (b - a);
 	}
 }
 
 namespace RCCar 
 {
-	MotorControl::MotorControl(int PWMPin, int DirectionPin)
-		: m_PWMPin(PWMPin), m_directionPin(DirectionPin), m_speed(0)
+	MotorControl::MotorControl(int PWMPin_FWD, int PWMPin_BWD)
+		: m_PWMPin_FWD(PWMPin_FWD), m_PWMPin_BWD(PWMPin_BWD), m_speed(0), m_currentSpeed(0)
 	{
-		pinMode(PWMPin, OUTPUT);
-		pinMode(DirectionPin, OUTPUT);
+		pinMode(m_PWMPin_FWD, OUTPUT);
+		pinMode(m_PWMPin_BWD, OUTPUT);
+		pinMode(m_measurePin, INPUT);
 	}
 
-	void MotorControl::setSpeed(int speed)
+	void MotorControl::setSpeed(int speed, bool lerpChange)
 	{
-		m_speed = speed;
+		m_speed = (double)speed + m_correctionFactor;
 	}
 
-	void MotorControl::setDirection(MotorDirection direction)
+	void MotorControl::setCorrectionFactor(int factor)
 	{
-		m_direction = direction;
+		m_correctionFactor = factor;
 	}
 
 	void MotorControl::run()
 	{
-		// lerp the speed
-		if (m_currentSpeed != m_speed)
+		m_currentSpeed = Utils::lerp(m_currentSpeed, m_speed, 0.01f);
+
+		// IF speed is 0, set all PWM pins to LOW
+		if (m_currentSpeed == 0) 
 		{
-			m_currentSpeed = Utils::lerp(m_currentSpeed, m_speed, 0.1f);
+			digitalWrite(m_PWMPin_FWD, LOW);
+			digitalWrite(m_PWMPin_BWD, LOW);
+			return;
 		}
 
-		analogWrite(m_PWMPin, m_currentSpeed);
+		if (m_speed > 0) 
+		{
+			analogWrite(m_PWMPin_FWD, (int)abs(m_currentSpeed));
+			digitalWrite(m_PWMPin_BWD, LOW);
+		}
+		else if (m_speed < 0)
+		{
+			digitalWrite(m_PWMPin_FWD, LOW);
+			analogWrite(m_PWMPin_BWD, (int)abs(m_currentSpeed));
+		}
 
-		// Set the direction of the motor
-		if (m_direction == MotorDirection::Clockwise)
-			digitalWrite(m_directionPin, HIGH);
-		else
-			digitalWrite(m_directionPin, LOW);
-
+		// Write the motor speed to HW.
+		//analogWrite((int)abs(m_speed) > 0 ? m_PWMPin_FWD : m_PWMPin_BWD, (int)abs(m_currentSpeed));
 	}
 }
--- RC-CAR/RCControlMGR.hpp
+++ RC-CAR/RCControlMGR.hpp
@@ -1,12 +1,15 @@
 #pragma once
+#include "SensorHLP.hpp"
 
 namespace RCCar 
 {
-	static constexpr int MOTOR_PIN_ACCELL_PWM = 6;
-	static constexpr int MOTOR_PIN_STEER_PWM = 5;
+	// Motor left PWM pin definitions
+	static constexpr int MOTOR_PIN_LEFT_PWM_FWD = 10; // B-IA
+	static constexpr int MOTOR_PIN_LEFT_PWM_BWD = 11; // B-IB
 
-	static constexpr int MOTORDRIVER_MAIN_DIRECTION_PIN = 10;
-	static constexpr int MOTORDRIVER_STEER_DIRECTION_PIN = 7;
+	// Motor right PWM pin definitions
+	static constexpr int MOTOR_PIN_RIGHT_PWM_FWD = 5; // A-IA
+	static constexpr int MOTOR_PIN_RIGHT_PWM_BWD = 6; // A-IB
 
 	enum MotorDirection 
 	{
@@ -24,17 +27,34 @@ namespace RCCar
 		/// <param name="DirectionPin">The Direction pin. This pin is connected to the H bridge.</param>
 		MotorControl(int PWMPin, int DirectionPin);
 
-		void setSpeed(int speed);
-		void setDirection(MotorDirection direction);
+		/// <summary>
+		/// Set the speed of the motor.
+		/// </summary>
+		/// <param name="speed">The speed of the motor. Has to be between -255 and 255.</param>
+		/// <param name="lerpChange">If false, the speed is set instantly, otherwise the speed is slowly set to the speed via a lerp function.</param>
+		void setSpeed(int speed, bool lerpChange = true);
 
+		void setCorrectionFactor(int factor);
+		int getCorrectionFactor() { return m_correctionFactor; }
+
+		/// <summary>
+		/// Measures the actual speed of the motor for WatchDog. Outputs the PWM signal to the motor and sets the direction by setting the DIR pin.
+		/// </summary>
 		void run();
 
-	private:
-		int m_PWMPin;
-		int m_directionPin;
-		int m_speed;
-		int m_currentSpeed;
+		/*static void SpeedMeasureInterruptHandler();*/
 
-		MotorDirection m_direction;
+	private:
+		int m_PWMPin_FWD;
+		int m_PWMPin_BWD;
+
+		double m_measurePin;
+		double m_speed;				// Goal speed, This is the speed that the motor should be at
+		double m_currentSpeed;		// The current speed of the motor. This is the lerped value.
+		int m_correctionFactor = 0;	// The multiplier for the correction of the speed.
+
+		unsigned long m_lastSpeedMeasureTime;	// The last time the speed was measured.
+
+		int m_measuredSpeed;	// The measured speed of the motor. This is the physical speed of the motor, read on the previous loop cycle.
 	};
 }
