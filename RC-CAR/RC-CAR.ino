b68bf5370ba96f8c82f4f71f516e189e52b6d8af
Davide_24 <009davide@gmail.com>
2025-01-17 11:44:46 +0100

    Introduce the RoboCar branch

--- RC-CAR/RC-CAR.ino
+++ RC-CAR/RC-CAR.ino
@@ -1,17 +1,29 @@
+#include <NewPing.h>
 #include "RCControlMGR.hpp"
 #include "RCBluetooth.hpp"
+#include "RoboCar.hpp"
+
+
+// THIS IS THE ROBOCAR BRANCH \\
 
 // motor controller object
-RCCar::MotorControl steerMotorControl(RCCar::MOTOR_PIN_STEER_PWM, RCCar::MOTORDRIVER_STEER_DIRECTION_PIN);
-RCCar::MotorControl accelMotorControl(RCCar::MOTOR_PIN_ACCELL_PWM, RCCar::MOTORDRIVER_MAIN_DIRECTION_PIN);
+RCCar::MotorControl accellRightMotorControl(RCCar::MOTOR_PIN_LEFT_PWM_FWD, RCCar::MOTOR_PIN_LEFT_PWM_BWD);
+RCCar::MotorControl accelLeftMotorControl(RCCar::MOTOR_PIN_RIGHT_PWM_FWD, RCCar::MOTOR_PIN_RIGHT_PWM_BWD);
 
-RCCar::BluetoothDevice bluetoothDevice("RC-CAR", 6);
+NewPing sonar(8, 9, 500);
+
+RCCar::RoboCar roboCar(accelLeftMotorControl, 2, accellRightMotorControl, 3, RCCar::Sensors::UltrasonicSensor(8, 9), RCCar::Sensors::UltrasonicSensor(8, 9));
+
+// BT is not yet available on this device
+//RCCar::BluetoothDevice bluetoothDevice("RC-CAR", 6);
 
 void setup() 
 {
 	while (!Serial);
 	Serial.begin(9600);
 
+	
+	
 	//---------------------------------------------- Set PWM frequency for D5 & D6 -------------------------------
 	//NOTE: Changing this timer 0 affects millis() and delay!
 	TCCR0B = TCCR0B & B11111000 | B00000001;    // set timer 0 divisor to     1 for PWM frequency of 62500.00 Hz    with this the motor doesn't whine
@@ -23,13 +35,8 @@ void setup()
 
 void loop() 
 {
-	bluetoothDevice.run();
+	//bluetoothDevice.run();
+	roboCar.run();
 
-	steerMotorControl.setDirection(*bluetoothDevice.getSteerDirection());
-	steerMotorControl.setSpeed(*bluetoothDevice.getMotorAngle());
-	steerMotorControl.run();
-
-	accelMotorControl.setDirection(*bluetoothDevice.getMotorDirection());
-	accelMotorControl.setSpeed(*bluetoothDevice.getMotorSpeed());
-	accelMotorControl.run();
+	//analogWrite(RCCar::MOTOR_PIN_LEFT_PWM_BWD, 255);
 }
